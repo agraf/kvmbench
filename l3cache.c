c3854d214bccfbe3c54155ae384fde0a2827a542
Alexander Graf <agraf@suse.de>
2017-09-01 14:10:28 +0200

    l3cache: sleep less

--- l3cache.c
+++ l3cache.c
@@ -3,17 +3,22 @@
 #include <stdint.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include <time.h>
 
 static void benchmark_sleeps(void)
 {
     struct timeval tv_start;
     struct timeval tv_end;
     struct timeval tv;
+    struct timespec ts_sleep;
     uint64_t loops = 0;
 
     assert(!gettimeofday(&tv_start, NULL));
     tv_end = tv_start;
     tv_end.tv_sec += 1;
+
+    ts_sleep.tv_nsec = 1;
+    ts_sleep.tv_sec = 0;
 
     while(1) {
         assert(!gettimeofday(&tv, NULL));
@@ -21,7 +26,7 @@ static void benchmark_sleeps(void)
             break;
 
         loops++;
-        usleep(1);
+        nanosleep(&ts_sleep, NULL);
     }
 
     printf("Number of sleeps: %ld\n", loops);
