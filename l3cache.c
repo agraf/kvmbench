a9d8537b4d518d92371a7760c8650d36fa203955
Alexander Graf <agraf@suse.de>
2017-09-01 17:29:14 +0200

    l3cache: switch to manual signals

--- l3cache.c
+++ l3cache.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <assert.h>
 #include <stdio.h>
 #include <stdint.h>
@@ -6,9 +7,33 @@
 #include <time.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/syscall.h>
 
-static pthread_cond_t cv;
-static pthread_mutex_t mutex;
+#if 0
+static int gettid(void)
+{
+    return syscall(__NR_gettid);
+}
+#endif
+
+volatile enum {
+    SIG_STATE_INIT,
+    SIG_STATE_IDLE,
+    SIG_STATE_SENDING,
+    SIG_STATE_RECEIVED,
+} sig_state = SIG_STATE_INIT;
+
+pthread_t threads[2];
+
+static void sig_handler(int sig)
+{
+//printf("[%d] handler %d\n", gettid(), sig);
+    if (sig == SIGUSR1)
+        sig_state = SIG_STATE_RECEIVED;
+}
 
 static void *waiter_thread(void *opaque)
 {
@@ -16,12 +41,23 @@ static void *waiter_thread(void *opaque)
     struct timeval tv_end;
     struct timeval tv;
     uint64_t loops = 0;
+    struct sigaction sa = {
+        .sa_handler = sig_handler,
+    };
+
+    sigset_t fSigSet;
+
+    sigemptyset(&fSigSet);
+    sigaddset(&fSigSet, SIGUSR1);
+    pthread_sigmask(SIG_UNBLOCK, &fSigSet, NULL);
+
+    sigaction(SIGUSR1, &sa, NULL);
 
     assert(!gettimeofday(&tv_start, NULL));
     tv_end = tv_start;
     tv_end.tv_sec += 1;
 
-    pthread_mutex_lock(&mutex);
+    sig_state = SIG_STATE_IDLE;
 
     while(1) {
         assert(!gettimeofday(&tv, NULL));
@@ -29,10 +65,11 @@ static void *waiter_thread(void *opaque)
             break;
 
         loops++;
-        pthread_cond_wait(&cv, &mutex);
+//printf("[%d] pre-received \n", gettid());
+        while (sig_state != SIG_STATE_RECEIVED) ;
+//printf("[%d] recevied\n", gettid());
+        sig_state = SIG_STATE_IDLE;
     }
-
-    pthread_mutex_unlock(&mutex);
 
     printf("Number of sleeps: %ld\n", loops);
     exit(0);
@@ -41,10 +78,12 @@ static void *waiter_thread(void *opaque)
 
 static void *notifier_thread(void *opaque)
 {
+    while (sig_state != SIG_STATE_IDLE) ;
     while (1) {
-        pthread_mutex_lock(&mutex);
-        pthread_cond_signal(&cv);
-        pthread_mutex_unlock(&mutex);
+//printf("[%d] trigger\n", gettid());
+        sig_state = SIG_STATE_SENDING;
+        pthread_kill(threads[1], SIGUSR1);
+        while (sig_state != SIG_STATE_IDLE) ;
     }
 
     return NULL;
@@ -52,8 +91,12 @@ static void *notifier_thread(void *opaque)
 
 int main(int argc, char **argv)
 {
-    pthread_t threads[2];
     int i;
+    sigset_t fSigSet;
+
+    sigemptyset(&fSigSet);
+    sigaddset(&fSigSet, SIGUSR1);
+    pthread_sigmask(SIG_BLOCK, &fSigSet, NULL);
 
     pthread_create(&threads[0], NULL, notifier_thread, NULL);
     pthread_create(&threads[1], NULL, waiter_thread, NULL);
