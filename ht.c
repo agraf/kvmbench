4fb7e0d44a7828cde86592ce88089903cb33e80b
Alexander Graf <agraf@suse.de>
2017-09-08 15:17:09 +0200

    ht: Properly analyze HT host structure

--- ht.c
+++ ht.c
@@ -6,8 +6,12 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 volatile uint32_t running = 0;
+int opt_pinned = 1;
 
 volatile struct {
     uint64_t result;
@@ -59,7 +63,9 @@ int pin_thread(pthread_t t, int core_id) {
 static void spawn_thread(pthread_t *pt, char *spawned, int is_alu, uintptr_t idx)
 {
     pthread_create(pt, NULL, is_alu ? thread_alu : thread_fpu, (void*)idx);
-    pin_thread(*pt, idx);
+    if (opt_pinned) {
+        pin_thread(*pt, idx);
+    }
     *spawned = 1;
 }
 
@@ -70,16 +76,71 @@ int main(int argc, char **argv)
     char thread_spawned[num_cores];
     pthread_t thread[num_cores];
     uint64_t result = 0;
+    char thread_siblings[1024];
+    char *tsp;
+    int is_alu = 0;
+
+    for (i = 1; i < argc; i++) {
+        if (!strcmp(argv[i], "-P")) {
+            opt_pinned = 0;
+        } else {
+            printf("Syntax: %s\n\n", argv[0]);
+            printf("  -P    Don't pin the threads\n");
+            printf("\n");
+            exit(0);
+        }
+    }
 
     results = malloc(num_cores * sizeof(*results));
     memset((void*)results, 0, num_cores * sizeof(*results));
     memset(thread_spawned, 0, num_cores);
 
-#if 0
-    char path[512];
+#if 1
 
     for (i = 0; i < num_cores; i++) {
-        snprintf(path, sizeof(path), "/sys/devices/system/cpu/cpu%d/topology/thread_siblings");
+        char path[512];
+        int fd;
+        int offset = 0;
+
+        if (thread_spawned[i]) continue;
+
+        snprintf(path, sizeof(path), "/sys/devices/system/cpu/cpu%d/topology/thread_siblings", i);
+
+        memset(thread_siblings, 0, sizeof(thread_siblings));
+        fd = open(path, O_RDONLY);
+        if (fd < 0) {
+            printf("Error opening %s\n", path);
+            return 1;
+        }
+        read(fd, thread_siblings, sizeof(thread_siblings));
+        /* Just hope we read everything */
+        close(fd);
+
+        tsp = thread_siblings + strlen(thread_siblings) - 9;
+        while (1) {
+            uint32_t cur_siblings;
+            int j;
+
+            if (sscanf(tsp, "%08x", &cur_siblings) != 1) {
+                printf("Error reading siblings from %s\n", tsp);
+                exit(1);
+            }
+
+            for (j = 0; j < 32; j++) {
+                if (cur_siblings & (1U << j)) {
+                    int cpu = j + offset;
+                    spawn_thread(&thread[cpu], &thread_spawned[cpu], is_alu, cpu);
+                    is_alu = (is_alu + 1) & 1;
+                }
+            }
+
+            if (tsp == thread_siblings) {
+                break;
+            }
+
+            tsp -= 9;
+            offset += 32;
+        }
     }
 #else
     for (i = 0; i < num_cores; i++) {
@@ -91,10 +152,10 @@ int main(int argc, char **argv)
         spawn_thread(&thread[thread0], &thread_spawned[thread0], 1, thread0);
         spawn_thread(&thread[thread1], &thread_spawned[thread1], 0, thread1);
     }
+#endif
 
     running = 1;
     sleep(5);
-#endif
 
     for (i = 0; i < num_cores; i++) {
         result += results[i].result;
