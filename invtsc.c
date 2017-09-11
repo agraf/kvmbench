87a0d1b7131ba35819c8c1300e27c2a40c375606
Alexander Graf <agraf@suse.de>
2017-09-11 13:34:17 +0200

    invtsc: Make multi-arch

--- invtsc.c
+++ invtsc.c
@@ -56,9 +56,17 @@ static void benchmark_gettimeofday(void)
 
 static __inline__ unsigned long long rdtsc(void)
 {
+#ifdef __x86_64__
     unsigned hi, lo;
     __asm__ __volatile__ ("rdtsc" : "=a"(lo), "=d"(hi));
     return ( (unsigned long long)lo)|( ((unsigned long long)hi)<<32 );
+#elif defined(__aarch64__)
+    unsigned long long ret;
+    asm volatile ("isb; mrs %0, cntvct_el0" : "=r" (ret));
+    return ret;
+#else
+#error Unsupported architecture
+#endif
 }
 
 static uint64_t tsc_per_sec;
